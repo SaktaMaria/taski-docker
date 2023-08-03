23230a8154b4f38025dc2eb1e0379fdda4a32597
Христич Мария <mary_khristitch@inbox.ru>
2023-08-03 20:13:33 +0300

    Add workflow with PEP8 linting 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -70,9 +70,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
